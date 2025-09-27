03c379a9da751c747535911f01255173cd6e801f
Happyanurag2001 <anuragsinghkushwahaaa@gmail.com>
2025-09-27 22:01:06 +0530

    extended

--- login/script.js
+++ login/script.js
@@ -13,28 +13,23 @@ form.addEventListener('submit', function(e){
     const passwordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?#&^])[A-Za-z\d@$!%*?#&^]{8,}$/;
     let emailAns = emailRegex.test(Uname.value);
     let passwordAns = passwordRegex.test(password.value);
-
     let isValid = true;
     if(!emailAns){
         document.querySelector("#emailError").textContent = "Please provide a valid email";
         document.querySelector("#emailError").style.display = "initial";
         isValid = false;
     }
-
     if(!passwordAns){
         document.querySelector("#passwordError").textContent = "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character.";
         document.querySelector("#passwordError").style.display = "initial";
         isValid = false;
 
     }
-
     if(isValid){
         let errorMessages = document.getElementById('errorMessages');
         document.querySelector(".error").style.display = "initial";
         form.submit();
     }
-
-
     console.log('form submitted');
 });
 
